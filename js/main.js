f5549bdde97ead4bd669507b38f2dd1b9027f802
ondoheer <ondoheer@gmail.com>
2019-10-11 15:58:07 -0500

    Ya guarda

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
+/**
+ * Elementos con los que trabajaremos
+ */
 var fechaContenedor = document.getElementById("fecha-container");
 
+var botonGuardar = document.getElementById("guardar");
+
+/**
+ *
+ * @param {HTMLElement} idHTML
+ * Remueve elementos HTMl del tree
+ */
+function borrarElementoHTML(idHTML) {
+  var elemento = document.getElementById(idHTML);
+  if (elemento) {
+    elemento.remove();
+  }
+}
+
+/**
+ * Crea el boton de eliminar para la fecha
+ */
 function crearBotonEliminar() {
   // si no lo encuentras crealo
   if (!document.getElementById("botonBorrado")) {
@@ -14,15 +34,49 @@ function crearBotonEliminar() {
   }
 }
 
-fechaContenedor.addEventListener("mouseenter", crearBotonEliminar);
+function guardarEstado() {
+  var data = {
+    fecha: "",
+    cuerpo: "",
+    timestamp: ""
+  };
 
+  // esto debería estar en otro lado
+  data.timestamp = Date.now();
+  data.fecha = document.getElementById("fecha").innerHTML;
+  data.cuerpo = document.getElementById("editor").innerHTML;
+
+  // convertimos a string
+  var dataAsString = JSON.stringify(data);
+  localStorage.setItem("data", dataAsString);
+}
+
+function cargarEstado() {
+  var localData = localStorage.getItem("data");
+  if (localData) {
+    localData = JSON.parse(localData);
+
+    if (
+      confirm(
+        `desea cargar la versión guardada del ${new Date(
+          localData.timestamp
+        ).toISOString()}`
+      )
+    ) {
+      document.getElementById("fecha").innerHTML = localData.fecha;
+      localData.cuerpo = document.getElementById("editor").innerHTML =
+        localData.cuerpo;
+    }
+  }
+}
+
+/**
+ * Crear event listeners
+ */
+fechaContenedor.addEventListener("mouseenter", crearBotonEliminar);
 fechaContenedor.addEventListener("mouseleave", function() {
   borrarElementoHTML("botonBorrado");
 });
+botonGuardar.addEventListener("click", guardarEstado);
 
-function borrarElementoHTML(idHTML) {
-  var elemento = document.getElementById(idHTML);
-  if (elemento) {
-    elemento.remove();
-  }
-}
+window.addEventListener("load", cargarEstado);
